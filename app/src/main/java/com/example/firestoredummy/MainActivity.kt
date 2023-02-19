2e1edd04bbf440a6a5389d0f0acdd95dc7814f58
aditya-appdesk <aditya@appdeskservices.com>
2023-02-20 04:10:16 +0530

    read and  converted excel to list

--- app/src/main/java/com/example/firestoredummy/MainActivity.kt
+++ app/src/main/java/com/example/firestoredummy/MainActivity.kt
@@ -7,6 +7,13 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import toast
 
+import android.content.Context
+import android.util.Log
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import java.io.InputStream
+
+
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -28,7 +35,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+       val data = readExcelFromRaw(this,R.raw.boooook)
+        Log.i("Data",data.toString())
     }
+
 
     private fun saveDataToFireStore(title: String, description: String) {
         val notes = hashMapOf(
@@ -42,4 +53,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun readExcelFromRaw(context: Context, resourceId: Int): List<List<String>> {
+        val inputStream: InputStream = context.resources.openRawResource(resourceId)
+        val workbook = HSSFWorkbook(inputStream)
+        val sheet = workbook.getSheetAt(0)
+        val rows = sheet.iterator()
+
+        val result = mutableListOf<List<String>>()
+        while (rows.hasNext()) {
+            val currentRow = rows.next()
+            val cells = currentRow.iterator()
+
+            val rowValues = mutableListOf<String>()
+            while (cells.hasNext()) {
+                val cell = cells.next()
+                rowValues.add(cell.toString())
+            }
+            result.add(rowValues)
+        }
+
+        return result
+    }
 }
