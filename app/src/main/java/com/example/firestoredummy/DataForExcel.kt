9bbf5d77b7b997045abba74973b68e67d6b45d86
aditya-appdesk <aditya@appdeskservices.com>
2023-02-21 19:20:52 +0530

    list arrange  by group

---
+++ app/src/main/java/com/example/firestoredummy/DataForExcel.kt
@@ -0,0 +1,9 @@
+package com.example.firestoredummy
+
+data class DataForExcel(
+    val bookName:String,
+    val authorName:String,
+    val publisherName:String,
+    val date:String,
+    val source:String
+)
--- app/src/main/java/com/example/firestoredummy/MainActivity.kt
+++ app/src/main/java/com/example/firestoredummy/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.firestoredummy
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firestoredummy.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import toast
-
-import android.content.Context
-import android.util.Log
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import toast
 import java.io.InputStream
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -47,34 +45,48 @@ class MainActivity : AppCompatActivity() {
             "Description" to description
         )
         db.collection("Notes").add(notes).addOnSuccessListener {
-     toast("Saved")
+            toast("Saved")
         }.addOnFailureListener {
             toast("Error")
         }
     }
 
-     fun readExcelFromRaw(context: Context, resourceId: Int): List<List<String>> {
+    private fun readExcelFromRaw(context: Context, resourceId: Int): List<DataForExcel> {
         val inputStream: InputStream = context.resources.openRawResource(resourceId)
         val workbook = HSSFWorkbook(inputStream)
         val sheet = workbook.getSheetAt(0)
-        val rows = sheet.iterator()
+        //   Log.d("taget",sheet.toMutableSet().toString())
+        /*    val rows = sheet.iterator()
 
-        val result = mutableListOf<List<String>>()
+            val result = mutableListOf<List<String>>()
 
-        while (rows.hasNext()) {
-            val currentRow = rows.next()
-            val cells = currentRow.iterator()
+            while (rows.hasNext()) {
+                val currentRow = rows.next()
+                val cells = currentRow.iterator()
 
-            val rowValues = mutableListOf<String>()
-            while (cells.hasNext()) {
-                val cell = cells.next()
-                if(cell.toString().isNotBlank()){
-                    rowValues.add(cell.toString())
+                val rowValues = mutableListOf<String>()
+                while (cells.hasNext()) {
+                    val cell = cells.next()
+                    if(cell.toString().isNotBlank()){
+                        rowValues.add(cell.toString())
+                    }
                 }
-            }
 
-            result.add(rowValues)
+                result.add(rowValues)
+            }
+            return result*/
+
+        val listOfResults = mutableListOf<DataForExcel>()
+        sheet.forEachIndexed { index, row ->
+            val data = mutableListOf<String>()
+            row.forEach {
+                if(it.toString().isNotBlank())
+                    data.add(it.toString())
+            }
+            val res = DataForExcel(data.get(0),data.get(1),data.get(2),data.get(3),data.get(4))
+            listOfResults.add(res)
         }
-        return result
+        Log.d("Answer",listOfResults.groupBy { it.bookName }.toString())
+        return listOfResults
     }
 }
