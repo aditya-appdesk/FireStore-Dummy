8112ee81ab1e6347075b78a31f5c231aba930ce8
aditya-appdesk <aditya@appdeskservices.com>
2023-02-22 15:24:15 +0530

    Created dynamic function to read Excel(xls file)

--- app/src/main/java/com/example/firestoredummy/MainActivity.kt
+++ app/src/main/java/com/example/firestoredummy/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.firestoredummy
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.firestoredummy.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -11,19 +18,14 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import toast
 import java.io.InputStream
 
-import android.Manifest
-import android.app.Activity
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val db = Firebase.firestore
     private val STORAGE_PERMISSION_CODE = 1
+    private val PICKFILE_REQUEST_CODE = 11
+    lateinit var uri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,11 +44,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val data = readExcelFromRaw(this, R.raw.boooook)
-        Log.i("Data", data.toString())
+        // val data = readExcelFromRaw(this, R.raw.boooook)
+        //   Log.i("Data", data.toString())
         binding.apply {
             uploadButton.setOnClickListener {
-                checkStoragePermission(this@MainActivity)
+                if (checkStoragePermission(this@MainActivity)) {
+                    showFilePicker(this@MainActivity)
+                }
             }
         }
     }
@@ -64,8 +68,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun readExcelFromRaw(context: Context, resourceId: Int): List<DataForExcel> {
-        val inputStream: InputStream = context.resources.openRawResource(resourceId)
+    private fun readExcelFromRaw(context: Context, fileUri: Uri): List<DataForExcel> {
+        val inputStream: InputStream? = context.contentResolver.openInputStream(fileUri)
         val workbook = HSSFWorkbook(inputStream)
         val sheet = workbook.getSheetAt(0)
         val listOfResults = mutableListOf<DataForExcel>()
@@ -82,19 +86,22 @@ class MainActivity : AppCompatActivity() {
         return listOfResults
     }
 
-    private fun checkStoragePermission(activity: Activity) {
+    private fun checkStoragePermission(activity: Activity): Boolean {
         if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
             != PackageManager.PERMISSION_GRANTED
         ) {
             // Permission is not granted, request for permission
+
             ActivityCompat.requestPermissions(
                 activity,
                 arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
                 STORAGE_PERMISSION_CODE
             )
+            return false
         } else {
             // Permission is granted, continue with your task
             // ...
+            return true
         }
     }
 
@@ -111,9 +118,43 @@ class MainActivity : AppCompatActivity() {
             } else {
                 // Permission is not granted, show a message or handle the failure
                 // ...
+
             }
+        }
     }
 
+    fun handleFilePickerResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        onFileSelected: (Uri) -> Unit
+    ) {
+        if (requestCode == PICKFILE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val uri: Uri? = data?.data
+            if (uri != null) {
+                onFileSelected(uri)
+                readExcelFromRaw(this@MainActivity, uri)
+            }
+        }
+    }
 
-}}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        handleFilePickerResult(requestCode, resultCode, data) { fileUri ->
+            // Do something with the selected file URI
+            uri = fileUri
+            Log.d("FilePicker", fileUri.toString())
+        }
+    }
+
+    fun showFilePicker(activity: Activity) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        val chooserIntent = Intent.createChooser(intent, "Select File")
+        activity.startActivityForResult(chooserIntent, PICKFILE_REQUEST_CODE)
+    }
+
+}
 
