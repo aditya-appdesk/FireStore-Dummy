c0761b403969a3c2af4b47be00867d1e60be63af
aditya-appdesk <aditya@appdeskservices.com>
2023-02-28 23:32:51 +0530

    Try Catch wraped

--- app/src/main/java/com/example/firestoredummy/CreateExcelFileActivity.kt
+++ app/src/main/java/com/example/firestoredummy/CreateExcelFileActivity.kt
@@ -1,10 +1,12 @@
 package com.example.firestoredummy
 
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firestoredummy.databinding.ActivityCreateExcelFileBinding
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import toast
 import java.io.File
 import java.io.FileOutputStream
 
@@ -21,23 +23,32 @@ class CreateExcelFileActivity : AppCompatActivity() {
     }
 
     fun createAndSave() {
-        // Create a new Excel file
-        val workbook = XSSFWorkbook()
-        val sheet = workbook.createSheet("Sheet1")
-        val row = sheet.createRow(0)
-        val cell = row.createCell(0)
-        cell.setCellValue("Hello")
+        try {
+            // Create a new Excel file
+            val workbook = XSSFWorkbook()
+            val sheet = workbook.createSheet("Sheet1")
+            val row = sheet.createRow(0)
+            val cell = row.createCell(0)
+            cell.setCellValue("Hello")
 
-// Save the Excel file to external storage
-        val filename = "myExcel.xlsx"
-        val file = File(getExternalFilesDir(null), filename)
-        val fileOutputStream = FileOutputStream(file)
-        workbook.write(fileOutputStream)
-        fileOutputStream.close()
+
+            val filename = "DemoExcel.xls"
+            val downloadsDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            val file = File(downloadsDirectory, filename)
+            val fileOutputStream = FileOutputStream(file)
+            workbook.write(fileOutputStream)
+            fileOutputStream.close()
 
 // Print the path of the Excel file
-        val filePath = file.absolutePath
-        Log.d("Excel File Path", filePath)
+            val filePath = file.absolutePath
+            Log.d("Excel File Path", filePath)
+            toast("Excel Created Successfully")
+        }
+        catch (e:Exception){
+            e.printStackTrace()
+            toast(e.message.toString())
+        }
+
     }
 
 }
